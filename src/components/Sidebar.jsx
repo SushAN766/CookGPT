55001a2c7b2ca560dccf2ef04d564a3c4d12277d
SushAN766 <sushanthbs766@gmail.com>
2025-01-05 22:48:16 +0530

    app completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <>
-      <DesktopSidebar />
-      <MobileSidebar />
-    </>
-  )
-}
-
+    	<>
+      		<DesktopSidebar />
+      		<MobileSidebar />
+    	</>
+  	);
+};
 export default Sidebar;
 
 const DesktopSidebar = () => {
@@ -17,10 +16,10 @@ const DesktopSidebar = () => {
     <div className='p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block'>
 			<div className='flex flex-col gap-20 sticky top-10 left-0'>
 				<div className='w-full'>
-                <img src='/logo.svg' alt='logo' className='hidden md:block' />
-					      <img src='/mobile-logo.svg' alt='logo' className='block md:hidden' />
-        </div>
-        <ul className='flex flex-col items-center md:items-start gap-8'>
+                		<img src='/logo.svg' alt='logo' className='hidden md:block' />
+					    <img src='/mobile-logo.svg' alt='logo' className='block md:hidden' />
+        		</div>
+        		<ul className='flex flex-col items-center md:items-start gap-8'>
 					<Link to={"/"} className='flex gap-1'>
 						<Home size={"24"} />
 						<span className='font-bold hidden md:block'>Home</span>
@@ -30,7 +29,6 @@ const DesktopSidebar = () => {
 						<span className='font-bold hidden md:block'>Favorites</span>
 					</Link>
 				</ul>
-
         </div>
     </div>
   ) 
